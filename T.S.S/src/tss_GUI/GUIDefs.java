227b1630e24a442d6ea95fa2e389678e65d87999
Yerodin Richards <yero_din@hotmail.com>
2014-11-24 00:23:33 -0500

    Create GUIDefs.java

---
+++ T.S.S/src/tss_GUI/GUIDefs.java
@@ -0,0 +1,18 @@
+package tss_GUI;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class GUIDefs
+{
+	final public static Color BACKDROP_COLOR = new Color(227, 229, 238);
+	final public static Color BOLD_TEXT_COLOR = new Color(20, 21, 170);
+	final public static Color COMMON_TEXT_COLOR = new Color(30, 43, 131);
+	final public static Font BASE_HEADING_FONT = new Font("Verdana",Font.BOLD,14);
+	final public static Font BASE_COMMON_FONT = new Font("Verdana",Font.PLAIN,12);
+	final public static Font BOLD_COMMON_FONT = new Font("Verdana",Font.BOLD,12);
+	final public static Color LIST_SELECTED_COLOR = new Color(50,100,189);
+
+}
