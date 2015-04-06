0999a9c840e4bc343c187e6e020b0813232aa419
Yerodin <yero_din@hotmail.com>
2015-04-05 20:09:31 -0500

    changed login info

--- WebServer/TSS/appScripts/db_config.php
+++ WebServer/TSS/appScripts/db_config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_USER","root");
-define("DB_PASSWORD", "1loveANA");
+define("DB_PASSWORD", "enter password");
 define("DB_NAME","tss_testdatabase");
 define("DB_SERVER","127.0.0.1");
 ?>	
